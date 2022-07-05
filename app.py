36b8134f6489d0bac4c903c456f66062cc219e02
trinhhungfischer <trinhhungfischer@gmail.com>
2022-07-05 18:02:56 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import traceback
+from flask import Flask, render_template, request, jsonify, Response
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=False) 
